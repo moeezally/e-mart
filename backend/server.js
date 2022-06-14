2bd6a55889233877ba8c01e5747dc5f8ec033cef
Mubashir-Faheem <92710130+Mubashir-Faheem@users.noreply.github.com>
2022-06-14 21:54:18 +0500

    fixing port issues

--- backend/server.js
+++ backend/server.js
@@ -53,6 +53,6 @@ app.use('(/*)?', async (req, res, next) => {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
